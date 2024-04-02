c875557b7f99561c41203044348fd2e4610753a5
MJ-Dev92 <minjae920802@naver.com>
2024-04-03 00:14:47 +0900

    set product to firebase

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { uploadImage } from "../api/uploader";
+import { addNewProduct } from "../api/firebase";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
@@ -17,7 +18,7 @@ export default function NewProduct() {
   const handleSubmit = (e) => {
     e.preventDefault();
     uploadImage(file).then((url) => {
-      console.log(url);
+      addNewProduct(product, url);
     });
   };
 
